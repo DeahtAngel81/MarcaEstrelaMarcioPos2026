e18eeea9e9f350a97f515ce6879bffe922bd4bce
Marcio R. Silva <mr3475995@gmail.com>
2026-08-12 19:49:29 -0300

    Projeto com JetPack Preferences completo.

--- app/src/main/java/com/marcioposgraduacao/marcaestrela/MainActivity.kt
+++ app/src/main/java/com/marcioposgraduacao/marcaestrela/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.marcioposgraduacao.marcaestrela
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import android.view.inputmethod.InputBinding
 import android.widget.Button
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlin.jvm.java
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var imagem: ImageView
     private lateinit var btOnOff: Button
-
+    private lateinit var btConfiguracoes: Button
     private var isChecked = false
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,27 +31,51 @@ class MainActivity : AppCompatActivity() {
 
         imagem = findViewById(R.id.imagem)
         btOnOff = findViewById(R.id.btOnOff)
+        btConfiguracoes = findViewById(R.id.btConfiguracoes)
+
+        sharedPreferences = getSharedPreferences("PREFERECE_FILE", MODE_PRIVATE)
+        isChecked = sharedPreferences.getBoolean("isChecked", false)
+
+        when (isChecked) {
+            false -> {
+                imagem.setImageResource(android.R.drawable.btn_star_big_off)
+            }
+
+            true -> {
+                imagem.setImageResource(android.R.drawable.btn_star_big_on)
+            }
+
+        }
 
         btOnOff.setOnClickListener {
             btOnOffOnClick()
+        }
 
+        btConfiguracoes.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
         }
     }
 
     private fun btOnOffOnClick() {
         // Poderia ter utilizado o if e else para o status da estrela.
+
         when (isChecked) {
             false -> {
                 imagem.setImageResource(android.R.drawable.btn_star_big_on)
                 isChecked = true
             }
+
             true -> {
                 imagem.setImageResource(android.R.drawable.btn_star_big_off)
                 isChecked = false
             }
-
         }
-
+        val editor = sharedPreferences.edit()
+        editor.putBoolean("isChecked", isChecked)
+        editor.apply()
     }
 
-} // fim da MainActivity
+}
+
+// fim da MainActivity
